42db08219900d01e13e34bdf8e75d5e28c3ff5f5
Sohan <sohanur471@gmail.com>
2024-06-09 06:06:20 +0600

    useeffect dependency

--- app/page.js
+++ app/page.js
@@ -2,7 +2,7 @@ import React from "react";
 import HeroAndOfferSection from "./components/home/heroSection";
 import FoodItems from "./components/home/foods";
 
-export default async function Page() {
+export default function Page() {
   return (
     <React.Fragment>
       <HeroAndOfferSection />
